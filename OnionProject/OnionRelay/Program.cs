56c907dca90f8af6ee5d072433419fe092f4c797
Krzysztof Niedźwiecki <k.niedzwiecki@hotmail.com>
2019-12-04 17:45:30 +0100

    created OnionRelay:  console based relay app

---
+++ OnionProject/OnionRelay/Program.cs
@@ -0,0 +1,29 @@
+﻿using OnionCore;
+using System;
+using System.Text;
+
+namespace OnionRelay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UInt16 targetPort;
+            try               { targetPort = UInt16.Parse(args[0]); }
+            catch (Exception) { targetPort = 5555; }
+
+            byte[] targetKey;
+            try              { targetKey = Convert.FromBase64String(args[1]); }
+            catch(Exception) { targetKey = Encoding.ASCII.GetBytes("default-bytes-from-ascii"); }
+
+            OnionEndpoint thisRelay = new OnionEndpoint() {
+                hostName = "127.0.0.1",
+                port = targetPort,
+                aesKey = targetKey
+            };
+
+            Console.WriteLine("Running OnionRelay at {0}:{1} with key {2}", thisRelay.hostName, thisRelay.port.ToString(), Convert.ToBase64String(thisRelay.aesKey));
+            OnionTransport.RunSecureServer(thisRelay, OnionMessaging.ExecuteReceivedCommand);
+        }
+    }
+}
