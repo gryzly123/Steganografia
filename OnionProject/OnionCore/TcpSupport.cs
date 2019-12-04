ac3c1fee889f5c35a840f8ac488ad8ed8ea6e0d3
Krzysztof Niedźwiecki <k.niedzwiecki@hotmail.com>
2019-12-04 09:05:46 +0100

    Core: de/serialization of commands, command detector and spawner method, de/serialization of endpoint data, TCP now supports both secure and insecure connections, message packer and handler class (OnionMessage)

--- OnionProject/OnionCore/TcpSupport.cs
+++ OnionProject/OnionCore/TcpSupport.cs
@@ -8,10 +8,34 @@ namespace OnionCore
 
     public class TcpSupport
     {
-        public static void SendData(OnionEndpoint targetRelay, byte[] inData, out byte[] outData)
+        public static void SendRawData(OnionEndpoint targetRelay, byte[] inData, out byte[] outData)
         {
             //create client
-            using (TcpClient client = new TcpClient(targetRelay.targetIp.ToString(), targetRelay.port))
+            using (TcpClient client = new TcpClient(targetRelay.hostName.ToString(), targetRelay.port))
+            {
+                NetworkStream stream = client.GetStream();
+
+                //send data
+                stream.Write(BitConverter.GetBytes((UInt32)inData.Length), 0, 4);
+                stream.Write(inData, 0, inData.Length);
+
+                //receive response
+                byte[] targetLen = new byte[4];
+                stream.Read(targetLen, 0, 4);
+                UInt32 bytes = BitConverter.ToUInt32(targetLen, 0);
+                outData = new byte[bytes];
+                stream.Read(outData, 0, (Int32)bytes);
+
+                //close client
+                stream.Close();
+                client.Close();
+            }
+        }
+
+        public static void SendSecureData(OnionEndpoint targetRelay, byte[] inData, out byte[] outData)
+        {
+            //create client
+            using (TcpClient client = new TcpClient(targetRelay.hostName, targetRelay.port))
             {
                 NetworkStream stream = client.GetStream();
 
@@ -33,8 +57,8 @@ namespace OnionCore
                 client.Close();
             }
         }
-
-        public static void RunServer(OnionEndpoint thisRelay, HandleTcpData onDataReceived)
+        
+        public static void RunSecureServer(OnionEndpoint thisRelay, HandleTcpData onDataReceived)
         {
             TcpListener server = null;
             try
@@ -74,8 +98,49 @@ namespace OnionCore
             finally
             {
                 //close server
-                if(server != null) server.Stop();
+                if (server != null) server.Stop();
             }
         }
+        
+        /*public static void RunServer(OnionEndpoint thisRelay, HandleTcpData onDataReceived)
+        {
+            TcpListener server = null;
+            try
+            {
+                //create server
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddr, thisRelay.port);
+                server.Start();
+
+                //handle clients
+                while (true)
+                {
+                    //receive client
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+
+                    //read data from client
+                    byte[] targetLen = new byte[4];
+                    stream.Read(targetLen, 0, 4);
+                    UInt32 bytes = BitConverter.ToUInt32(targetLen, 0);
+                    byte[] inData = new byte[bytes];
+                    stream.Read(inData, 0, (Int32)bytes);
+
+                    //handle data and send response
+                    onDataReceived(inData, out byte[] outData);
+                    stream.Write(BitConverter.GetBytes((UInt32)inData.Length), 0, 4);
+                    stream.Write(outData, 0, outData.Length);
+
+                    //close client
+                    client.Close();
+                }
+            }
+            finally
+            {
+                //close server
+                if(server != null) server.Stop();
+            }
+        }*/
+
     }
 }
